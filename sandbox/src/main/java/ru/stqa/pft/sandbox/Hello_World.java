7762a791651e1610ae5c5ddf40847ffc297a79a4
MaryGoo <m381988@gmail.com>
2020-11-29 08:29:55 +0300

    add a lot of functional

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Hello_World.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Hello_World.java
@@ -2,6 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class Hello_World {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("Word");
+		hello("Mary");
+
+		double l = 5;
+    System.out.println("Площадь квдрата =" + area(l));
+
+    double a = 1;
+    double b = 3;
+    System.out.println("Площадь прямоугольника =" + area(a, b));
+
 	}
+	public static void hello(String some) {
+    System.out.println("Hello, " + some + "!");
+  }
+
+  public static double area (double lkv) {
+	  return lkv * lkv;
+  }
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
