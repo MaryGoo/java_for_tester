cff979e6c538558e33ce6ddf56a66a04de29886a
MaryGoo <m381988@gmail.com>
2020-11-30 20:07:12 +0300

    new testDistanceBetweenPoint  with assertEquals, delta

--- sandbox/src/test/java/ru/stqa/pft/distanceBetweenPoint/DistanceBetweenPointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/distanceBetweenPoint/DistanceBetweenPointTests.java
@@ -3,28 +3,38 @@ package ru.stqa.pft.distanceBetweenPoint;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class DistanceBetweenPointTests {
   @Test
   public void testDistanceBetweenPoint0() {
     Point p1 = new Point(4,4);
     Point p2 = new Point(4,4);
-    Assert.assertEquals(Distance.distance(p1,p2), 0);
+    assertEquals(Distance.distance(p1,p2), 0);
   }
 
   @Test
   public void testDistanceBetweenPoint1() {
     Point p1 = new Point(4,4);
     Point p2 = new Point(1,1);
-    double expectedResult = Math.round((3 * Math.sqrt(2))*100.0) / 100.0;
-    double actualResult = Math.round (Distance.distance(p1,p2) * 100.0) / 100.0;
-    Assert.assertEquals(actualResult, expectedResult);
+    double expectedResult = 3 * Math.sqrt(2);
+    double actualResult = Distance.distance(p1,p2);
+    assertEquals(expectedResult, actualResult, 0.001);
   }
   @Test
   public void testDistanceBetweenPoint2() {
     Point p1 = new Point(0,4);
     Point p2 = new Point(2,0);
-    double expectedResult = Math.round((2 * Math.sqrt(5))*100.0) / 100.0;
-    double actualResult = Math.round (Distance.distance(p1,p2) * 100.0) / 100.0;
-    Assert.assertEquals(actualResult, expectedResult);
+    double expectedResult = (2 * Math.sqrt(5));
+    double actualResult = Distance.distance(p1,p2);
+    assertEquals(expectedResult, actualResult, 0.001);
+  }
+  @Test
+  public void testDistanceBetweenPoint3() {
+    Point p1 = new Point(-4,-4);
+    Point p2 = new Point(-2,0);
+    double expectedResult = (2 * Math.sqrt(5));
+    double actualResult = Distance.distance(p1,p2);
+    assertEquals(expectedResult, actualResult, 0.001);
   }
 }
