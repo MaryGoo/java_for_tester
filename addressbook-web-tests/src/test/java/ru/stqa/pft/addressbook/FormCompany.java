7561a4ba463a43719dbc14d53043ffa7541ec038
MaryGoo <m381988@gmail.com>
2020-12-07 06:34:30 +0300

    TestContactCreation not end

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/FormCompany.java
@@ -0,0 +1,31 @@
+﻿package ru.stqa.pft.addressbook;
+
+public class FormCompany {
+  private final String company;
+  private final String address;
+  private final String homePhone;
+  private final String mobilePhone;
+
+  public FormCompany(String company, String address, String homePhone, String mobilePhone) {
+    this.company = company;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.mobilePhone = mobilePhone;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/FormForNewAddressbookName.java
@@ -0,0 +1,37 @@
+﻿package ru.stqa.pft.addressbook;
+
+public class FormForNewAddressbookName {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+
+  public FormForNewAddressbookName(String firstname, String middlename, String lastname, String nickname, String title) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+}
