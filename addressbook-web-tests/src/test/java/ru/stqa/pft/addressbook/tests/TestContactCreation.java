0218f2dc7e8f21659724976c593e925c26e1227a
MaryGoo <m381988@gmail.com>
2020-12-09 08:37:22 +0300

    TestContactCreation step 1
    просто скопирован из рекодера и  подправлен . запускается

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestContactCreation.java
@@ -4,13 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
-import ru.stqa.pft.addressbook.model.*;
 
 public class TestContactCreation {
   private WebDriver wd;
-
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
@@ -18,90 +17,75 @@ public class TestContactCreation {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String usename, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(usename);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
-  public void testContactCreation() throws Exception {
-    goToNewAddressbook();
-    fillFormForNewAddressbook(new FormForNewAddressbookName("maryghhs", "mfghix", "ghsfghoo", "marsfhyGoo", "titleMshhsary"));
-    fillCompany(new FormCompany("sfdhsfgh", "HGJHGJshfhfhg 154/8", "353535", "8989885454"));
-    fillFormEmail(new FormMail("hfjk1111g@fgh.ru", "email"));
-    fillFormEmail(new FormMail("hfjkg11122@fgh.ru", "email2"));
-    fillFormEmail(new FormMail("hfjkg111133@fgh.ru", "email3"));
-    fillFormHomePage(new FormHomePage("kgh111kgjh.ru"));
-    fillDate(new FormDate("a", "5", "March", "2000"));
-    fillDate(new FormDate("b", "10", "March", "2020"));
-    fillGroup(new FormGroupByAddNew("test3"));
-    submitAddNewContact();
-    goToHomePage();
-    logout();
-  }
-
-
-
-  private void goToNewAddressbook() {
+  public void TestContactCreationCase() throws Exception {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
     wd.findElement(By.linkText("add new")).click();
-  }
-
-  private void fillFormForNewAddressbook(FormForNewAddressbookName formForNewAddressbookName) {
+    wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(formForNewAddressbookName.getFirstname());
+    wd.findElement(By.name("firstname")).sendKeys("Casha");
+    wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(formForNewAddressbookName.getMiddlename());
+    wd.findElement(By.name("middlename")).sendKeys("by");
+    wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(formForNewAddressbookName.getLastname());
+    wd.findElement(By.name("lastname")).sendKeys("Lastname");
+    wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(formForNewAddressbookName.getNickname());
+    wd.findElement(By.name("nickname")).sendKeys("Cash");
+    wd.findElement(By.name("title")).click();
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(formForNewAddressbookName.getTitle());
-  }
-  private void fillCompany(FormCompany formCompany){
+    wd.findElement(By.name("title")).sendKeys("Cashtitle");
+    wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(formCompany.getCompany());
+    wd.findElement(By.name("company")).sendKeys("MMTR");
+    wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(formCompany.getAddress());
+    wd.findElement(By.name("address")).sendKeys("street Shagova 180-5/1");
+    wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(formCompany.getHomePhone());
+    wd.findElement(By.name("home")).sendKeys("325525");
+    wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(formCompany.getMobilePhone());
-  }
-    private void fillFormEmail(FormMail formMail) {
-      wd.findElement(By.name(formMail.getMailNumber())).clear();
-      wd.findElement(By.name(formMail.getMailNumber())).sendKeys(formMail.getMailValue());
-    }
-  private void fillFormHomePage(FormHomePage formHomePage){
-      wd.findElement(By.name("homepage")).clear();
-      wd.findElement(By.name("homepage")).sendKeys(formHomePage.getNameHomePage());
-    }
-     private void fillDate(FormDate formDate) {
-      new Select(wd.findElement(By.name(formDate.getGroupDate() +"day"))).selectByVisibleText(formDate.getDay());
-      new Select(wd.findElement(By.name(formDate.getGroupDate() +"month"))).selectByVisibleText(formDate.getMonth());
-      wd.findElement(By.name(formDate.getGroupDate() +"year")).clear();
-      wd.findElement(By.name(formDate.getGroupDate() +"year")).sendKeys(formDate.getYear());
-    }
-  private void fillGroup(FormGroupByAddNew formGroupByAddNew) {
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(formGroupByAddNew.getNameGroup());
-  }
-
-  private void submitAddNewContact(){
+    wd.findElement(By.name("mobile")).sendKeys("+7989989898");
+    wd.findElement(By.name("work")).click();
+    wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys("-7985656565");
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("csh@gmail.ru");
+    wd.findElement(By.name("email2")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("cash@gmail.ru");
+    wd.findElement(By.name("email2")).click();
+    wd.findElement(By.name("email2")).clear();
+    wd.findElement(By.name("email2")).sendKeys("cash2@mail.ru");
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("1");
+    wd.findElement(By.name("bday")).click();
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("January");
+    wd.findElement(By.name("bmonth")).click();
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys("1999");
+    wd.findElement(By.xpath("//body")).click();
+    wd.findElement(By.name("new_group")).click();
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("test4");
+    wd.findElement(By.name("new_group")).click();
+    wd.findElement(By.xpath("//body")).click();
     wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-  }
-
-  private void goToHomePage() {
     wd.findElement(By.linkText("home")).click();
-  }
-
-  private void logout() {
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
@@ -126,4 +110,5 @@ public class TestContactCreation {
       return false;
     }
   }
+
 }
