56aa9f88bddd587e20b30d5a708e75c755dd4e60
MaryGoo <mgupsa@mmtr.ru>
2021-01-04 12:30:41 +0300

    4.3 check the size of elements BEFORE and AFTER the test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -9,9 +10,13 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
    // app.getNavigationHelper().goToAddNewPage();
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().createContact(new ContactData("Casha", "by", "Lastname", "Cash", "Cashtitle", "MMTR", "street Shagova 180-5/1", "325525", "+7985656565", "csh@gmail.ru","test4"),true);
     //app.getContactHelper().fillAddNewContact(new ContactData("Casha", "by", "Lastname", "Cash", "Cashtitle", "MMTR", "street Shagova 180-5/1", "325525", "+7985656565", "csh@gmail.ru","test4"),true);
    // app.getContactHelper().submitAddContact();
+    app.getNavigationHelper().goToHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before + 1);
 
   }
 }
