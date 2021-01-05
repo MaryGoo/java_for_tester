b55b133522631be540426e387c56f924f2a0e7ef
MaryGoo <m381988@gmail.com>
2021-01-05 17:19:09 +0300

    5.2 BeforeMethod in GroupDeletion/Modification Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,28 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
-  @Test
-  public void testGroupModification(){
+
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().goToGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test4", null, null));
     }
+  }
+
+  @Test
+  public void testGroupModification(){
     List<GroupData> before = app.getGroupHelper().getGroupList();
     int index = before.size()- 1;
     GroupData group = new GroupData(before.get(index).getId(), "test254", "454545","68764");
-    app.getGroupHelper().selectGroup(index);
-    app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupModification();
-    app.getGroupHelper().returnGroupPage();
+    app.getGroupHelper().modifyGroup(index, group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
 
